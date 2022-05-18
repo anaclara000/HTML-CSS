27b37cae3d04bd82d18194cee1f39378193980c7
AnaaClaraa <anaclara130305@gmail.com>
2022-05-18 11:25:55 -0300

    18/05

---
+++ bootstrap/index.js
@@ -0,0 +1,7 @@
+const btnToast = document.getElementById ("btnToast");
+const toast = document.getElementById ("toast");
+    btnToast.addEventListener('click', () => {
+        const seutoast = new bootstrap.Toast(toast);
+
+        seutoast.show();
+    })
